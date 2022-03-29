66943b3641f6319c5ba0d193ca6f77c18ba98d54
Connor Stack <cstack@Connors-MBP.attlocal.net>
2022-03-29 14:12:13 -0400

    Add a fourth puzzle

--- src/PuzzleLibrary.js
+++ src/PuzzleLibrary.js
@@ -57,8 +57,9 @@ const PUZZLE2 = [
 ];
 
 const PUZZLE3 = [[{"filled":false,"hint":2},{"filled":false,"hint":3},{"hint":null},{"filled":false,"hint":2},{"hint":null},{"hint":null},{"filled":false,"hint":1}],[{"hint":null},{"hint":null},{"filled":false,"hint":3},{"hint":null},{"filled":true,"hint":6},{"hint":null},{"filled":true,"hint":3}],[{"hint":null},{"filled":false,"hint":5},{"hint":null},{"filled":false,"hint":3},{"hint":null},{"hint":null},{"hint":null}],[{"filled":true,"hint":3},{"hint":null},{"hint":null},{"hint":null},{"hint":null},{"hint":null},{"filled":false,"hint":5}],[{"filled":false,"hint":3},{"hint":null},{"hint":null},{"hint":null},{"filled":true,"hint":4},{"hint":null},{"hint":null}],[{"hint":null},{"filled":false,"hint":4},{"hint":null},{"filled":true,"hint":5},{"hint":null},{"hint":null},{"filled":false,"hint":4}],[{"filled":false,"hint":2},{"hint":null},{"hint":null},{"filled":true,"hint":4},{"hint":null},{"hint":null},{"filled":false,"hint":2}]];
+const PUZZLE4 = [[{"hint":null},{"filled":true,"hint":5},{"hint":null},{"filled":true,"hint":3},{"hint":null},{"hint":null},{"filled":false,"hint":3}],[{"hint":null},{"hint":null},{"hint":null},{"hint":null},{"hint":null},{"hint":null},{"hint":null}],[{"hint":null},{"filled":true,"hint":3},{"hint":null},{"hint":null},{"filled":false,"hint":5},{"hint":null},{"filled":false,"hint":3}],[{"hint":null},{"hint":null},{"hint":null},{"filled":false,"hint":5},{"filled":false,"hint":4},{"hint":null},{"hint":null}],[{"filled":true,"hint":3},{"hint":null},{"filled":false,"hint":5},{"hint":null},{"hint":null},{"hint":null},{"filled":false,"hint":0}],[{"hint":null},{"hint":null},{"hint":null},{"hint":null},{"filled":true,"hint":2},{"hint":null},{"hint":null}]];
 
-const ALL_WITH_KEYS = { PUZZLE1, PUZZLE2, PUZZLE3 };
+const ALL_WITH_KEYS = { PUZZLE1, PUZZLE2, PUZZLE3, PUZZLE4 };
 const ALL = Object.values(ALL_WITH_KEYS);
 
 export default { ALL: ALL, ...ALL_WITH_KEYS };
